27139d442f8cac4c8a47287e47db78a178e3e154
Becquet <mdevries@domoti.fr>
2020-08-17 11:56:13 +0200

    creuser sur l'utilisation de redux-thunk @see s/index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,6 +3,7 @@ import axios from 'axios'
 export const GET_COUNTRIES = 'GET_COUNTRIES'
 
 export function fetchCountries() {
+  // dispatch is a redux-thunk @see src/index.js
   return function (dispatch) {
     axios.get('https://restcountries.eu/rest/v2/all')
       .then(axiosResponse => {
